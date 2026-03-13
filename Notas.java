ceb92176318a3f4f6341386c5b3a55dfa6c99b41
Luiga_luis <luis.miguel.flauzino@gmail.com>
2026-03-13 16:56:48 -0300

    atualizado exercicios das notas

--- Notas.java
+++ Notas.java
@@ -6,10 +6,10 @@ public class Notas {
 
         for(int i=0;i<students.length;i++){
 
-            System.out.println("Digite a nota do aluno" + i);
+            System.out.println("Digite a nota do aluno " + i);
             students[i] = entrada.nextInt();
 
-            if(students[i]<0 || students[i]<10){
+            if(students[i]<0 || students[i]>10){
                 System.out.println("Entrada invalida, insira o valor denovo");
                 i--;
             }
@@ -18,16 +18,42 @@ public class Notas {
     }
 
     public static int[] increseMarks(int[] students){
-        for(int t : students){
-            t = t+3;
+
+        for(int i=0; i< students.length ;i++){
+
+            students[i] = students[i] +3;
+
+            if(students[i] >10){
+                students[i] = 10;
+            }
+
+            System.out.println("Aluno" + i +": Nota:" + students[i]);
         }
         return students;
+    }
+
+    public static void allHavePassed(int[] students){
+        for (int i=0;i<students.length;i++) {
+            int j= i+1;
+            if(students[i] >= 5){
+                System.out.println("Aluno "+ j + ": Aprovado");
+            }
+            else{
+                System.out.println("Aluno " + j+ ": Reprovado");
+            }
+        }
     }
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
         int[] javaStudents = new int[4];
+
+        enterExamMarks(entrada, javaStudents);
+
+        javaStudents = increseMarks(javaStudents);
+
+        allHavePassed(javaStudents);
 
         entrada.close();
     }
